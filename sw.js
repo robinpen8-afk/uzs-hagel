a80aebffe7f218a1ab97614115f4b3db7136ebda
robinpen8-afk <robinpen8@gmail.com>
2026-06-25 17:13:01 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'uzs-hagel-v1';
+const CACHE = 'uzs-hagel-v2';
 const ASSETS = [
   '/uzs-hagel/',
   '/uzs-hagel/index.html',
@@ -22,14 +22,11 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => {
-      if (cached) return cached;
-      return fetch(e.request).then(res => {
-        if (!res || res.status !== 200 || res.type === 'opaque') return res;
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
-      }).catch(() => caches.match('/uzs-hagel/index.html'));
-    })
+    fetch(e.request).then(res => {
+      if(!res || res.status !== 200 || res.type === 'opaque') return res;
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
   );
 });
